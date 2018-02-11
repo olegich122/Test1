a105e0f20b880fa5445edc76feb7f9b85a9ec611
Olegich <olegich122@gmail.com>
2018-02-11 18:03:13 +0200

    test1

---
+++ app/src/main/java/com/xyz/App.java
@@ -0,0 +1,12 @@
+
+package com.xyz;
+
+import com.xyz.util.*;
+
+public class App {
+
+        public static void main(String[] args) {
+                System.out.println("Hello World! Today is " + DateUtil.getToday());
+        }
+
+}
---
+++ util/src/main/java/com/xyz/util/DateUtil.java
@@ -0,0 +1,13 @@
+
+package com.xyz.util;
+
+import java.util.Date;
+import org.apache.commons.lang.time.DateFormatUtils;
+
+public class DateUtil {
+
+        public static String getToday() {
+                String today = DateFormatUtils.format(new Date(), "dd-MMM-yyyy");
+                return today;
+        }
+}
